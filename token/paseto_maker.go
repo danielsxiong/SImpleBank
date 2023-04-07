f1868973693814733e7181c4a770613e7c89b2c7
Teguh <danielsxiong@gmail.com>
2023-04-07 21:48:24 +0700

    Create and verify JWT & PASETO

---
+++ token/paseto_maker.go
@@ -0,0 +1,51 @@
+package token
+
+import (
+	"fmt"
+	"github.com/aead/chacha20poly1305"
+	"time"
+
+	"github.com/o1egl/paseto"
+)
+
+// PASETOMaker is a JSON Web Token maker
+type PASETOMaker struct {
+	paseto       *paseto.V2
+	symmetricKey []byte
+}
+
+// NewPASETOMaker creates a new PASETOMaker
+func NewPASETOMaker(symmetricKey string) (Maker, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be at least %d characters", chacha20poly1305.KeySize)
+	}
+
+	return &PASETOMaker{
+		paseto:       paseto.NewV2(),
+		symmetricKey: []byte(symmetricKey),
+	}, nil
+}
+
+func (maker PASETOMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+	return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+}
+
+func (maker PASETOMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
