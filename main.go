de8a092e6ddc81683e2838adb1c792a662c7a9c4
Teguh <danielsxiong@gmail.com>
2023-05-20 08:58:29 +0700

    Run db migrations directly inside golang

--- main.go
+++ main.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"embed"
+	"io/fs"
+	"log"
+	"net"
+	"net/http"
+
 	"danielsxiong/simplebank/api"
 	db "danielsxiong/simplebank/db/sqlc"
 	"danielsxiong/simplebank/gapi"
 	"danielsxiong/simplebank/pb"
 	"danielsxiong/simplebank/util"
-	"embed"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/golang/mock/mockgen/model"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
-	"io/fs"
-	"net"
-	"net/http"
-
-	"database/sql"
-	"log"
 )
 
 //go:embed doc/swagger
@@ -35,9 +39,24 @@ func main() {
 		log.Fatal("cannot connect to db: ", err)
 	}
 
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 	go runGatewayServer(config, store)
 	runGRPCServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance", err)
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up", err)
+	}
+
+	log.Println("db migrated successfully")
 }
 
 func runGRPCServer(config util.Config, store db.Store) {
--- util/config.go
+++ util/config.go
@@ -8,6 +8,7 @@ import (
 type Config struct {
 	DBDriver             string        `mapstructure:"DB_DRIVER"`
 	DBSource             string        `mapstructure:"DB_SOURCE"`
+	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
 	HttpServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
 	GrpcServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
 	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
@@ -22,6 +23,7 @@ func LoadConfig(path string) (config Config, err error) {
 
 	viper.SetDefault("DB_DRIVER", "postgres")
 	viper.SetDefault("DB_SOURCE", "postgresql://root:ab@localhost:5432/simple_bank?sslmode=disable")
+	viper.SetDefault("MIGRATION_URL", "file://db/migration")
 	viper.SetDefault("HTTP_SERVER_ADDRESS", "0.0.0.0:8080")
 	viper.SetDefault("GRPC_SERVER_ADDRESS", "0.0.0.0:9090")
 	viper.SetDefault("TOKEN_SYMMETRIC_KEY", "12345678901234567890123456789012")
