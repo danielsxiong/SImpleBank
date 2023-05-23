4ba9c0aec537bdaf74a33aca218fe7cb22ff4306
Teguh <danielsxiong@gmail.com>
2023-05-23 19:35:23 +0700

    Update API with optional param

---
+++ gapi/rpc_update_user.go
@@ -0,0 +1,83 @@
+package gapi
+
+import (
+	"context"
+	db "danielsxiong/simplebank/db/sqlc"
+	"danielsxiong/simplebank/pb"
+	"danielsxiong/simplebank/util"
+	"danielsxiong/simplebank/val"
+	"database/sql"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"time"
+)
+
+func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	violations := validateUpdateUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
+	arg := db.UpdateUserParams{
+		Username: req.GetUsername(),
+		Email: sql.NullString{
+			String: req.GetEmail(),
+			Valid:  req.GetEmail() != "",
+		},
+		FullName: sql.NullString{
+			String: req.GetFullName(),
+			Valid:  req.GetFullName() != "",
+		},
+	}
+
+	if req.GetPassword() != "" {
+		hashedPassword, err := util.HashPassword(req.GetPassword())
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err.Error())
+		}
+		arg.HashedPassword = sql.NullString{
+			String: hashedPassword,
+			Valid:  true,
+		}
+		arg.PasswordChangedAt = sql.NullTime{
+			Time:  time.Now(),
+			Valid: true,
+		}
+	}
+
+	user, err := server.store.UpdateUser(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+		}
+		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err.Error())
+	}
+
+	resp := &pb.UpdateUserResponse{
+		User: convertUser(user),
+	}
+	return resp, nil
+}
+
+func validateUpdateUserRequest(req *pb.UpdateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+	if req.GetPassword() != "" {
+		if err := val.ValidatePassword(req.GetPassword()); err != nil {
+			violations = append(violations, fieldViolation("password", err))
+		}
+	}
+	if req.GetFullName() != "" {
+		if err := val.ValidateFullName(req.GetFullName()); err != nil {
+			violations = append(violations, fieldViolation("full_name", err))
+		}
+	}
+	if req.GetEmail() != "" {
+		if err := val.ValidateEmail(req.GetEmail()); err != nil {
+			violations = append(violations, fieldViolation("email", err))
+		}
+	}
+	return violations
+}
